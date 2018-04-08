89b20bde702be4caa0c7dc0b9d79d00bec8ea797
Luke Trynchuk <luke@silverbackgames.com>
2018-04-08 17:30:21 -0300

    Add Standalone Input Services.

---
+++ pkas/Assets/Scripts/Input/StandaloneController/StandaloneControllerInputService.cs
@@ -0,0 +1,79 @@
+﻿namespace GreenApple.Poke.Input
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using GreenApple.Poke.Core.Services;
+
+	public class StandaloneControllerInputService : MonoBehaviour , IInputService
+	{
+
+		#region Private Variables
+		private const string SELECT_BUTTON = "Select";
+		private const string CANCEL_BUTTON = "Cancel";
+		private const string MENU_BUTTON = "Menu";
+
+		private const string LEFT_STICK_HOR = "LeftStickHorizontal";
+		private const string LEFT_STICK_VER = "LeftStickVertical";
+		#endregion
+
+		#region Main Methods
+		void OnEnable()
+		{
+			RegisterService ();
+		}
+
+		public Vector2 GetMovementVector()
+		{
+			float xaxis = Input.GetAxis (LEFT_STICK_HOR);
+			float yaxis = Input.GetAxis (LEFT_STICK_VER);
+
+			return new Vector2 (xaxis, yaxis);
+		}
+
+		public bool PressedSelect()
+		{
+			return ButtonPressed (SELECT_BUTTON);
+		}
+
+		public bool PressedCancel()
+		{
+			return ButtonPressed (CANCEL_BUTTON);
+		}
+
+		public bool PressedMenu()
+		{
+			return ButtonPressed (MENU_BUTTON);
+		}
+
+		public void RegisterService()
+		{
+			#if UNITY_STANDALONE
+				ServiceLocator.Register<IInputService>(this);
+			#else
+				gameObject.SetActive (false);
+			#endif
+		}
+		#endregion
+
+		#region Utility Methods
+
+		private bool ButtonPressed(string ButtonName)
+		{
+			string osName;
+			#if UNITY_STANDALONE_OSX
+			osName = "_OSX";
+			#elif UNITY_STANDALONE_WIN
+			osName = "_WIN";
+			#elif UNITY_STANDALONE_LINUX
+			osName = "_LIN";
+			#endif
+
+			if (Input.GetButtonDown (ButtonName + osName))
+				return true;
+
+			return false;
+		}
+		#endregion
+	}
+}
---
+++ pkas/Assets/Scripts/Input/StandaloneInputServiceManager.cs
@@ -0,0 +1,76 @@
+﻿namespace GreenApple.Poke.Input
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	public class StandaloneInputServiceManager : MonoBehaviour 
+	{
+
+		#region Private Variables
+		[SerializeField]
+		private GameObject m_standaloneController;
+		[SerializeField]
+		private GameObject m_standaloneKeyboard;
+		private bool m_controllerConnected;
+		#endregion
+
+		#region Main Methods
+		void Start()
+		{
+			if (ControllerConnected ()) 
+			{
+				m_controllerConnected = true;
+				m_standaloneController.SetActive (true);
+				m_standaloneKeyboard.SetActive (false);
+			}
+
+			if (!ControllerConnected ()) 
+			{
+				m_controllerConnected = false;
+				m_standaloneController.SetActive (false);
+				m_standaloneKeyboard.SetActive (true);
+			}
+		}
+
+		void Update () 
+		{
+			SwitchStandaloneServiceMethod ();
+		}
+
+		#endregion
+
+		#region Utility Methods
+		private void SwitchStandaloneServiceMethod()
+		{
+			#if UNITY_STANDALONE
+			if(m_controllerConnected && !ControllerConnected())
+			{
+				m_controllerConnected = false;
+				m_standaloneController.SetActive(false);
+				m_standaloneKeyboard.SetActive(true);
+			}
+
+			if(!m_controllerConnected && ControllerConnected())
+			{
+				m_controllerConnected = true;
+				m_standaloneController.SetActive(true);
+				m_standaloneKeyboard.SetActive(false);
+			}
+			#endif
+		}
+		
+		#endregion
+
+		#region Low Level Functions
+
+		private bool ControllerConnected()
+		{
+			if (Input.GetJoystickNames ().Length > 0)
+				return true;
+			return false;
+		}
+				
+		#endregion
+	}
+}
