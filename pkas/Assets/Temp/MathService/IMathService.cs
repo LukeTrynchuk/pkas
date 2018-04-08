305325f2cd99c6e0a69e22d3c29b40010af37292
Luke Trynchuk <luke@silverbackgames.com>
2018-04-08 17:54:04 -0300

    Add Working Standalone Keyboard & Controller runtime switching.

--- pkas/Assets/Temp/MathService/IMathService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using GreenApple.Poke.Core.Services;
-
-public interface IMathService : IService
-{
-	int DoMath(int a, int b);
-}
---
+++ pkas/Assets/Temp/TEMP_InputLogger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GreenApple.Poke.Core.Services;
+using GreenApple.Poke.Input;
+
+public class TEMP_InputLogger : MonoBehaviour 
+{
+
+	#region Private Variables
+	private ServiceReference<IInputService> m_inputReference;
+	#endregion
+
+	void Start () 
+	{
+		m_inputReference = new ServiceReference<IInputService> ();
+	}
+
+	void Update () 
+	{
+		if (!m_inputReference.isRegistered ()) 
+		{
+			return;
+		}
+
+		if (m_inputReference.Reference.PressedSelect ())
+			Debug.Log ("SELECT");
+
+		if (m_inputReference.Reference.PressedCancel ())
+			Debug.Log ("CANCEL");
+
+		if (m_inputReference.Reference.PressedMenu ())
+			Debug.Log ("MENU");
+	}
+}
