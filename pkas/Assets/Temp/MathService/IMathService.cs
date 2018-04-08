4abef9ea9e4dd62e7da8b2f95e97b84421fc73f4
Luke Trynchuk <luke@silverbackgames.com>
2018-04-08 15:12:28 -0300

    Implement Service Locator API

---
+++ pkas/Assets/Temp/MathService/IMathService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GreenApple.Poke.Core.Services;
+
+public interface IMathService : IService
+{
+	int DoMath(int a, int b);
+}
---
+++ pkas/Assets/Temp/MathService/MathServiceB/MathAddService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GreenApple.Poke.Core.Services;
+
+public class MathAddService : MonoBehaviour , IMathService
+{
+
+	void Awake()
+	{
+		RegisterService ();
+	}
+
+	public int DoMath(int a, int b)
+	{
+		return a + b;
+	}
+
+	public void RegisterService ()
+	{
+		ServiceLocator.Register<IMathService> (this);
+	}
+}
