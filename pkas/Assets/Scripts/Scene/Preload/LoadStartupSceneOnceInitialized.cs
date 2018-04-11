9f37001813e0306ebad09cb40ab04d2cd7fbe19c
Luke Trynchuk <luke@silverbackgames.com>
2018-04-11 00:03:12 -0300

    Add Audio to intro. Add SceneManager service.

---
+++ pkas/Assets/Scripts/Scene/Preload/LoadStartupSceneOnceInitialized.cs
@@ -0,0 +1,30 @@
+﻿namespace GreenApple.Poke.Scene
+{
+
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using UnityEngine.SceneManagement;
+	using GreenApple.Poke.Core.Services;
+	using GreenApple.Poke.Scene;
+
+	public class LoadStartupSceneOnceInitialized : MonoBehaviour 
+	{
+		#region Private Variables
+		private ServiceReference<ISceneManager> m_sceneManager = new ServiceReference<ISceneManager>();
+		#endregion
+
+		#region Main Methods
+		void Start()
+		{
+			m_sceneManager.AddRegistrationHandle (HandleSceneManagerRegistered);
+		}
+
+		void HandleSceneManagerRegistered()
+		{
+			m_sceneManager.Reference.ProceedToNextScene ();
+		}
+
+		#endregion
+	}
+}
---
+++ pkas/Assets/Scripts/Timeline/StartAnimation_TimelineManager.cs
@@ -0,0 +1,48 @@
+﻿namespace GreenApple.Poke.Timeline
+{
+
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using UnityEngine.Timeline;
+	using UnityEngine.Playables;
+	using GreenApple.Poke.Core.Services;
+	using GreenApple.Poke.Scene;
+
+	[RequireComponent(typeof(PlayableDirector))]
+	public class StartAnimation_TimelineManager : MonoBehaviour 
+	{
+
+		#region Private Variables
+		PlayableDirector m_director;
+		private ServiceReference<ISceneManager> m_sceneManager = new ServiceReference<ISceneManager>();
+		#endregion
+
+
+		#region Main Methods
+
+		void Start()
+		{
+			m_director = GetComponent<PlayableDirector> ();
+		}
+
+		void Update()
+		{
+			if (!m_director.playableGraph.IsValid()) 
+			{
+				ChangeScene ();
+			}
+		}
+
+		#endregion
+
+		#region Utility Methods
+		void ChangeScene()
+		{
+			if (m_sceneManager.isRegistered ())
+				m_sceneManager.Reference.ProceedToNextScene ();
+		}
+			
+		#endregion
+	}
+}
